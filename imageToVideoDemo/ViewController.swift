7a231e04ba124f416acbe68046d24f40272eec29
Ujjwal Shrestha <ujjwal.shrestha@xtrastaff.com>
2020-02-11 22:06:57 +0545

    Initial Commit

---
+++ imageToVideoDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  imageToVideoDemo
+//
+//  Created by Xtrastaff on 11/2/20.
+//  Copyright © 2020 Xtrastaff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
